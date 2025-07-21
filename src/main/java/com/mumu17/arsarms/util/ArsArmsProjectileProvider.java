6e376b07ede935c31db3fa67f39546ae96ecf6aa
mumu17-git <moonpapa1709@gmail.com>
2025-07-21 23:27:29 +0900

    Version 1.2.1

--- src/main/java/com/mumu17/arsarms/util/ArsArmsProjectileProvider.java
+++ src/main/java/com/mumu17/arsarms/util/ArsArmsProjectileProvider.java
@@ -3,32 +3,33 @@ package com.mumu17.arsarms.util;
 import com.mumu17.castlib.util.IProjectileDataProvider;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.BlockHitResult;
 
 public class ArsArmsProjectileProvider implements IProjectileDataProvider {
     @Override
-    public boolean isEnabled() {
+    public boolean isEnabled(LivingEntity entity) {
         return ArsArmsProjectileData.isEnabled();
     }
 
     @Override
-    public Entity getTargetEntity() {
+    public Entity getTargetEntity(LivingEntity entity) {
         return ArsArmsProjectileData.getTargetEntity();
     }
 
     @Override
-    public InteractionHand getHand() {
+    public InteractionHand getHand(LivingEntity entity) {
         return ArsArmsProjectileData.getHand();
     }
 
     @Override
-    public BlockHitResult getBlockHitResult() {
+    public BlockHitResult getBlockHitResult(LivingEntity entity) {
         return ArsArmsProjectileData.getBlockHitResult();
     }
 
     @Override
-    public ItemStack getCurrentGun() {
+    public ItemStack getCurrentGun(LivingEntity entity) {
         return ArsArmsProjectileData.getCurrentGun();
     }
 }
